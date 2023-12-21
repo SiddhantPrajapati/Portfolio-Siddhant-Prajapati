5546aa21e67525fda1ae17504cb0b5232bb5b0b1
Siddhant <siddhantprajapati6377@gmail.com>
2023-12-21 16:36:25 +0530

    adding resume in index.html

--- src/app.js
+++ src/app.js
@@ -4,10 +4,14 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../public/index.html'));
+})
+
+app.get('/resume', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../contents/files/Siddhant Prajapati.pdf'))
 })
 
 app.listen(PORT, ()=> {
