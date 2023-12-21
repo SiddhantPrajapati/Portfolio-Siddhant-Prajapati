5531dd244fb45ad7843acd143c5e414e755c21f2
Siddhant <siddhantprajapati6377@gmail.com>
2023-12-21 09:07:50 +0530

    change: add script of start

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const PORT = 3000
 app.use(express.static(path.join(__dirname)))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../index.html'));
+    res.sendFile(path.resolve(__dirname, '../public/index.html'));
 })
 
 app.listen(PORT, ()=> {
