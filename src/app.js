cc6299a28e72435cbdeea817859924b853ebbb2b
Siddhant <siddhantprajapati6377@gmail.com>
2023-12-22 23:26:53 +0530

    add achievement field

--- src/app.js
+++ src/app.js
@@ -4,14 +4,32 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 
+//below given line is used public dictory for rrendering content
 app.use(express.static(path.join(__dirname, '../public')));
 
+//render index.html file in the root path
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../public/index.html'));
 })
 
+//below path is used for render internship certificate
+app.get('/achivement/internship', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../contents/files/Internship_Certificate.pdf'))
+})
+
+//below path show resume
 app.get('/resume', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../contents/files/Siddhant Prajapati.pdf'))
+})
+
+//below path is used to show ISRO Certification
+app.get('/achivement/ISROCertificate', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../contents/files/ISRO_Certificate.pdf'))
+})
+
+//below path is used to show certificate of Research paper
+app.get('/achivement/ResearchPaper', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../contents/files/Reserch_Paper_Certificate.pdf'))
 })
 
 app.listen(PORT, ()=> {
